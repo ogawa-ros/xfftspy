1c37b3f2856550ed7b64a53de35df22af50d9fbc
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-15 11:24:23 +0900

    #1 Fix package description specification

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import setuptools
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
 
 setuptools.setup(
     name = 'xfftspy',
     version = "0.1.3",
     description = '',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     url = 'https://github.com/ogawa-ros/xfftspy',
     author = 'Nishimura, Atsushi',
     author_email = 'ars096@gmail.com',
